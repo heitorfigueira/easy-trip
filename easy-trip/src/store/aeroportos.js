6cc5ec0229539534d0d1dd80d945ea0d5d51a51f
heitor.figueira <heitor.figueira@pine.com>
2020-07-06 07:55:22 -0300

    DSID de cu é rola

---
+++ easy-trip/src/store/aeroportos.js
@@ -0,0 +1,41 @@
+import skyscanner from '../../api/skyscanner.js'
+
+const aeroportos = {
+  namespaced: true,
+  state: {
+    listaAeroportos: [],
+    listaPassagens: [],
+  },
+  mutations: {
+    SET_LISTA_AEROPORTOS(state, value) {
+      state.listaAeroportos = value
+    },
+    SET_LISTA_PASSAGENS(state, value) {
+      state.listaPassagens = value
+    }
+  },
+  actions: {
+    async listarAeroportos(context, { query }) {
+      console.log(query)
+      let result = await skyscanner.places(query)
+      console.log('resultadoListarAeroportos', result)
+      let aeroportos = result.data.airports
+
+      context.commit('SET_LISTA_AEROPORTOS', aeroportos)
+    },
+    async listarPassagens(context, { query }) {
+      console.log(query)
+      let result = await skyscanner.flights(query)
+      console.log('resultadoListarPassagens', result)
+      let passagens = result.data
+
+      context.commit('SET_LISTA_PASSAGENS', passagens)
+    }
+  },
+  getters: {
+    listaAeroportos: (state) => state.listaAeroportos,
+    listaPassagens: (state) => state.listaPassagens,
+  },
+}
+
+export default aeroportos
